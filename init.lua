712999a59e1b6632ca5b89c2614b8e8e3da10dfc
Bradley <90872694+Bituvo@users.noreply.github.com>
2025-07-02 13:05:54 -0400

    Clean up, make it easier to add new animals

--- init.lua
+++ init.lua
@@ -3,49 +3,51 @@
 -- Inspired by OgelGames's "Be a Chicken" LUA command block on pandorabox.io
 
 local priv_name = "therianthrophy"
-
 local transformed_players = {}
--- Model, texture
-local player_model_data = {
-    chicken = {"mobs_chicken.b3d", "mobs_chicken.png"},
-    bunny = {"mobs_bunny.b3d", "mobs_bunny_grey.png"}
+
+local transformations = {
+    chicken = {
+        model = "mobs_chicken.b3d",
+        texture = "mobs_chicken.png",
+        register = function(self)
+            player_api.register_model(self.model, {
+                animation_speed = 24,
+                textures = {self.texture},
+                collisionbox = {-0.3, -0.75, -0.3, 0.3, 0.1, 0.3},
+                eye_height = 0,
+                animations = {
+                    stand = {x = 1, y = 30},
+                    lay = {x = 1, y = 30},
+                    walk = {x = 71, y = 90},
+                    mine = {x = 31, y = 70},
+                    walk_mine = {x = 91, y = 110},
+                    sit = {x = 1, y = 30}
+                }
+            })
+        end
+    },
+
+    bunny = {
+        model = "mobs_bunny.b3d",
+        texture = "mobs_bunny_grey.png",
+        register = function(self)
+            player_api.register_model(self.model, {
+                animation_speed = 15,
+                textures = {self.texture},
+                collisionbox = {-0.268, -0.5, -0.268, 0.268, 0.167, 0.268},
+                eye_height = 0,
+                animations = {
+                    stand = {x = 1, y = 15},
+                    lay = {x = 1, y = 15},
+                    walk = {x = 16, y = 24},
+                    mine = {x = 16, y = 24},
+                    walk_mine = {x = 16, y = 24},
+                    sit = {x = 1, y = 15}
+                }
+            })
+        end
+    }
 }
-
-minetest.register_on_leaveplayer(function(player)
-    transformed_players[player:get_player_name()] = nil
-end)
-
-local chicken_data = player_model_data.chicken
-player_api.register_model(chicken_data[1], {
-    animation_speed = 24,
-    textures = {chicken_data[2]},
-    collisionbox = {-0.3, -0.75, -0.3, 0.3, 0.1, 0.3},
-    eye_height = 0,
-    animations = {
-        stand = {x = 1, y = 30},
-        lay = {x = 1, y = 30},
-        walk = {x = 71, y = 90},
-        mine = {x = 31, y = 70},
-        walk_mine = {x = 91, y = 110},
-        sit = {x = 1, y = 30}
-    }
-})
-
-local bunny_data = player_model_data.bunny
-player_api.register_model(bunny_data[1], {
-    animation_speed = 15,
-    textures = {bunny_data[2]},
-    collisionbox = {-0.268, -0.5, -0.268, 0.268, 0.167, 0.268},
-    eye_height = 0,
-    animations = {
-        stand = {x = 1, y = 15},
-        lay = {x = 1, y = 15},
-        walk = {x = 16, y = 24},
-        mine = {x = 16, y = 24},
-        walk_mine = {x = 16, y = 24},
-        sit = {x = 1, y = 15}
-    }
-})
 
 -- Override the skin update function to skip chicken players,
 -- so that the custom properties do not get changed
@@ -75,30 +77,19 @@ local function set_model(player, model_name)
     })
 end
 
-local function to_human(player)
-    local name = player:get_player_name()
-    transformed_players[name] = nil
-    set_model(player, "skinsdb_3d_armor_character_5.b3d")
-    skins.update_player_skin(player)
-end
-
-local function transform(player, model_data)
+local function transform(player, model, texture)
     local name = player:get_player_name()
     transformed_players[name] = true
-    local model, texture = unpack(model_data)
     set_model(player, model)
     player_api.set_textures(player, {texture})
     -- Pop the player up a node so they dont fall into the node below
     player:set_pos(vector.add(player:get_pos(), vector.new(0, 1, 0)))
-
-    return true, ""
 end
 
 minetest.register_privilege(priv_name, {
     description = "Allows player to turn other players into animals.",
     give_to_singleplayer = false
 })
-
 
 minetest.register_chatcommand("human", {
     params = "[<player>]",
@@ -114,11 +105,15 @@ minetest.register_chatcommand("human", {
         if not player then
             return false, "Player does not exist."
         end
-        to_human(player)
+
+        local name = player:get_player_name()
+        transformed_players[name] = nil
+        set_model(player, "skinsdb_3d_armor_character_5.b3d")
+        skins.update_player_skin(player)
     end
 })
 
-for short_name, model_data in pairs(player_model_data) do
+for short_name, object in pairs(transformations) do
     core.register_chatcommand(short_name, {
         params = "[<player>]",
         description = "Turns player into a " .. short_name,
@@ -133,7 +128,18 @@ for short_name, model_data in pairs(player_model_data) do
             if not player then
                 return false, "Player is not logged in."
             end
-            transform(player, model_data)
+            transform(player, object.model, object.texture)
         end
     })
 end
+
+core.register_on_mods_loaded(function()
+    for short_name, object in pairs(transformations) do
+        object:register()
+    end
+end)
+
+minetest.register_on_leaveplayer(function(player)
+    transformed_players[player:get_player_name()] = nil
+end)
+
